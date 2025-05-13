bdcff288662fbd8f5fceb6a7190f8e0d7120ff15
Joaquin Esteban <dev@joacoesteban.com>
2025-05-13 21:39:06 +0200

    ADD timeline

---
+++ app/components/Timeline.tsx
@@ -0,0 +1,13 @@
+export default function Timeline() {
+  return (
+    <>
+      <a
+        className="twitter-timeline"
+        href="https://twitter.com/morfleps?ref_src=twsrc%5Etfw"
+      >
+        Tweets by morfleps
+      </a>
+      <script async src="https://platform.twitter.com/widgets.js"></script>
+    </>
+  );
+}
