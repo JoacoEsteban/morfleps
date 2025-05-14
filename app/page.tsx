c4350af51c18f71be1d80c4f7bc4e04c0f76506b
Joaquin Esteban <dev@joacoesteban.com>
2025-05-14 02:13:57 +0200

    Title

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,12 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1 className="max-md:text-[12vw] md:text-[8rem] font-bold text-center w-full relative">
-          MORFLEPS
-          <span className="absolute rotate-12 scale-50 -top-15 -right-5">
-            ✌️
+        <h1 className="max-md:text-[12vw] md:text-[8rem] font-bold text-center w-full">
+          <span className="relative">
+            MORFLEPS
+            <span className="absolute rotate-12 scale-50 top-5 right-0 translate-x-[50%]">
+              ✌️
+            </span>
           </span>
         </h1>
         <h2 className="w-full text-center max-md:text-[5vw] md:text-2xl">
